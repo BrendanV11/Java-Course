3ac10067ddb7379788fcbe15e6875efc0991d9b3
BrendanV11 <88327992+BrendanV11@users.noreply.github.com>
2022-08-20 21:59:37 +0200

    Added files
    Char and Boolean

---
+++ char and boolean/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    char myChar='D';
+        char uniCode='\u0044'; //backslash u specifies unicode char value.
+        System.out.println(uniCode);
+        System.out.println(myChar);
+
+        char copyRight='\u00A9';
+        System.out.println(copyRight);
+
+        boolean myTrueVal=true;
+        boolean myFalseVal=false;
+
+        boolean isCustomerOver21=true;
+    }
+}
