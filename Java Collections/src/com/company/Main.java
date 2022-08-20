c8d27045b00b0655320cf1a14c7ddffd325dc6d9
BrendanV11 <88327992+BrendanV11@users.noreply.github.com>
2022-08-20 22:10:39 +0200

    Added files
    Java Collections

---
+++ Java Collections/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre= new Theatre("Olympian",8,12);
+        //theatre.getSeats();
+
+        if(theatre.reserveSeat("H11")){
+            System.out.println("Please pay");
+        }else{
+            System.out.println("Seat is taken");
+        }
+    }
+}
