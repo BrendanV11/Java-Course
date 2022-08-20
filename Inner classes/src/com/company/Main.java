fbc10d49d42c4044a778ffa13b107d5e1befbc3b
BrendanV11 <88327992+BrendanV11@users.noreply.github.com>
2022-08-20 22:08:00 +0200

    Added files
    Inner Classes

---
+++ Inner classes/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Gearbox mcLaren = new Gearbox(6);
+
+
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+    }
+}
