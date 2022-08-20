207fd789044e544b670da789ff3e2c7938c7780b
BrendanV11 <88327992+BrendanV11@users.noreply.github.com>
2022-08-20 22:03:24 +0200

    Added files
    Encapsulation

---
+++ Encapsulation/src/com/company/Player.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Player {
+//    public String name;
+//    public int health;
+//    public String weapon;
+//
+//    public void loseHealth(int damage)
+//    {
+//        this.health=this.health-damage;
+//        if(this.health<=0)
+//        {
+//            System.out.println("Player knocked out");
+//// Reduce number of lives of player
+//        }
+//    }
+//    public int healthRemaining()
+//    {
+//        return this.health;
+//    }
+}
